0f502f6973ec49716f219fa911e9c13a41e052cd
Stéphane Champailler <schampailler@skynet.be>
2020-10-05 16:24:01 +0200

    added one simple test

--- Tps/SimplexeExoSup/simplex_method.jl
+++ Tps/SimplexeExoSup/simplex_method.jl
@@ -207,12 +207,19 @@ function simplex_method(c, A, b)
 end
 
 
-c = [-3; -2; -1; -5; 0; 0; 0]
-A = [7 3 4 1 1 0 0 ;
-     2 1 1 5 0 1 0 ;
-     1 4 5 2 0 0 1 ]
-b = [7; 3; 8]
+# c = [-3; -2; -1; -5; 0; 0; 0]
+# A = [7 3 4 1 1 0 0 ;
+#      2 1 1 5 0 1 0 ;
+#      1 4 5 2 0 0 1 ]
+# b = [7; 3; 8]
+
+# simplex_method(c, A, b)
 
 
 
-simplex_method(c, A, b)
+c = [-3; -2; 0; 0; 0]
+A = [2 1 1 0 0;
+     2 3 0 1 0;
+     3 1 0 0 1 ]
+b = [18; 42; 24]
+simplex_method(c, A, b) # expectd result is 33 => good !
