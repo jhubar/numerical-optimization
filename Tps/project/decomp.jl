aae65f9f1294ae8139869e18cccb1f1718c17333
Stéphane Champailler <schampailler@skynet.bewq>
2020-12-03 17:22:57 +0100

    impreovments

--- Tps/project/decomp.jl
+++ Tps/project/decomp.jl
@@ -2,47 +2,97 @@ using JuMP
 using Gurobi
 #using MosekTools
 using ImageView
+using Images
 
 include("utilities.jl")
 
 
 PATH = "/mnt/data2/uliege/optimisation/data/"
-measurements = unpickler( string(PATH, "uncorrupted_measurements_M1014.pickle"))
-measurement_matrix = unpickler( string(PATH, "measurement_matrix_M1014.pickle"))
-sparsifying_matrix = unpickler( string(PATH, "basis_matrix.pickle"))
+
+MEASUREMENTS = [608 1014 1521 3042]
+
+for measurement_id in MEASUREMENTS
+
+  reconstruct_name_base = "uncorrupted_$(measurement_id).png"
+  measurements = unpickler( string(PATH, "uncorrupted_measurements_M$(measurement_id).pickle"))
+  measurement_matrix = unpickler( string(PATH, "measurement_matrix_M$(measurement_id).pickle"))
+  sparsifying_matrix = unpickler( string(PATH, "basis_matrix.pickle"))
+
+  print("Working on $(reconstruct_name_base)")
+
+  print(size(sparsifying_matrix))
+  print(size(measurement_matrix))
+  print(size(measurements))
+
+  c_matrix = measurement_matrix * sparsifying_matrix
+  print(size(c_matrix))
 
 
+  R, C = size(c_matrix)
 
-print(size(sparsifying_matrix))
-print(size(measurement_matrix))
-print(size(measurements))
+  # https://jump.dev/JuMP.jl/stable/constraints/#Vectorized-constraints-1
 
-c_matrix = measurement_matrix * sparsifying_matrix
-print(size(c_matrix))
+  # L1 Norm ------------------------------------------------------------
+  # LP_model = Model(Gurobi.Optimizer)
+
+  @variable(LP_model, x[i=1:C]) # The sparse vector we're looking for
+  @constraint(LP_model, matrix_c, c_matrix * x .== measurements)
+
+  # Expressing objective function as L1 norm
+  # https://support.gurobi.com/hc/en-us/community/posts/360056614871-L1-norm-in-objective-function
+
+  @variable(LP_model, t[i=1:C] >= 0)
+  @constraint(LP_model, x .<= t) # . make comparing each x_i to each t_i
+  @constraint(LP_model, -x .<= t)
+  @variable(LP_model, t_sum >= 0)
+  @constraint(LP_model, sum(t) == t_sum) # sum(t) = t_0 + t_1 + t_2 + ... This is here 'cos stc wasn't able to put it directly in the objective function
+
+  idata = reshape( sparsifying_matrix * value.(x), 78, 78)
+  save( string("L1_",reconstruct_name_base), colorview(Gray,idata))
+  imshow(idata)
 
 
-R, C = size(c_matrix)
+  # Closed-form solution to the l2-norm problem ------------------------
 
-# https://jump.dev/JuMP.jl/stable/constraints/#Vectorized-constraints-1
+  # LP_model2 = Model(Gurobi.Optimizer)
+  # @variable(LP_model2, x[i=1:C]) # The sparse vector we're looking for
 
-LP_model = Model(Gurobi.Optimizer)
+  # # The sparse vector must satisfy the compressi ve sensing constraints:
+  # @constraint(LP_model2, matrix_c, c_matrix * x .== measurements)
+
+  # # The sparse vector must be minimized according to L2 norm
+  # # To achieve that, we express the norm "into" a second order cone.
+  # @variable(LP_model2, t >= 0)
+  # @constraint(LP_model2, epigraph, vcat(t, x) in SecondOrderCone())
+
+  # # By minimizing t_sum, we actually minimize the L2 norm
+  # @objective(LP_model2, Min, t)
+
+  # optimize!(LP_model2)
+
+  # idata = reshape( sparsifying_matrix * value.(x), 78, 78)
+  # imshow(idata)
+  # save( string( "L2_",reconstruct_name_base), colorview(Gray,idata))
 
 
-@variable(LP_model, x[i=1:C])
-@constraint(LP_model, matrix_c, c_matrix * x .== measurements)
+  # Expressing as Robust L1 norm -------------------------------------
 
-# Expressing objective function as L1 norm
-# https://support.gurobi.com/hc/en-us/community/posts/360056614871-L1-norm-in-objective-function
+  # LP_model3 = Model(Gurobi.Optimizer)
 
-@variable(LP_model, t[i=1:C] >= 0)
-@constraint(LP_model, x .<= t)
-@constraint(LP_model, -x .<= t)
-@variable(LP_model, t_sum >= 0)
-@constraint(LP_model, sum(t) == t_sum)
+  # epsilon = [0.01 for i in 1:C]
 
-@objective(LP_model, Min, t_sum)
+  # @variable(LP_model3, x[i=1:C]) # The sparse vector we're looking for
 
-optimize!(LP_model)
-idata = reshape( sparsifying_matrix * value.(x), 78, 78)
+  # # Constraint + allowing a bit of room for errors
+  # @constraint(LP_model3, matrix_c, (c_matrix * x - measurements) / measurements .<= epsilon )
 
-imshow(idata)
+  # # Expressing L1 Norm
+  # @variable(LP_model3, t[i=1:C] >= 0)
+  # @constraint(LP_model3, x .<= t) # . make comparing each x_i to each t_i
+  # @constraint(LP_model3, -x .<= t)
+  # @variable(LP_model3, t_sum >= 0)
+  # @constraint(LP_model3, sum(t) == t_sum) # sum(t) = t_0 + t_1 + t_2 + ..
+  # @objective(LP_model3, Min, t)
+
+  # optimize!(LP_model3)
+end
